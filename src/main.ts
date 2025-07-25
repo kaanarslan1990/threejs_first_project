7845828a8c5d363b9b812f5c6255e636795a41fc
okarslan <okarslan@dabnet.com.tr>
2025-07-26 01:17:12 +0300

    scene exmples

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,21 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import Stats from 'three/addons/libs/stats.module.js'
 import { GUI } from 'dat.gui'
 
-const scene = new THREE.Scene()
+const sceneA = new THREE.Scene()
+sceneA.background = new THREE.Color(0x123456) // Set background color to black
+
+const sceneB = new THREE.Scene()
+sceneB.background = new THREE.TextureLoader().load('https://sbcode.net/img/grid.png')
+
+const sceneC = new THREE.Scene()
+sceneC.background = new THREE.CubeTextureLoader()
+  .setPath('https://sbcode.net/img/')
+  .load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'])
+  //scene.backgroundBlurriness = 0.5 // Set background blurriness
+
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z =1.5
+camera.position.z = 1.5
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -25,23 +36,28 @@ const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshNormalMaterial({ wireframe: true })
 
 const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+sceneA.add(cube)
 
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
+let activeScene = sceneA
+const setScene = {
+  sceneA: () => {
+    activeScene = sceneA
+  },
+  sceneB: () => {
+    activeScene = sceneB
+  },
+  sceneC: () => {
+    activeScene = sceneC
+  },
+}
+
 const gui = new GUI()
-const cubeFolder = gui.addFolder('Cube Properties')
-
-
-cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2).name('Rotate X')
-cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2).name('Rotate Y')
-cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2).name('Rotate Z')
-cubeFolder.open()
-
-const cameraFolder = gui.addFolder('Camera Properties')
-cameraFolder.add(camera.position, 'z', 0, 20).name('Position X')
-cameraFolder.open()
+gui.add(setScene, 'sceneA').name('Scene A')
+gui.add(setScene, 'sceneB').name('Scene B')
+gui.add(setScene, 'sceneC').name('Scene C')
 
 function animate() {
   requestAnimationFrame(animate)
@@ -49,7 +65,7 @@ function animate() {
   // cube.rotation.x += 0.01
   // cube.rotation.y += 0.01
 
-  renderer.render(scene, camera)
+  renderer.render(activeScene, camera)
   stats.update()
 }
 
