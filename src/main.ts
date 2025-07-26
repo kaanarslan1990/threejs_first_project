bd43aeeb14e9a8c01d74b5564d48a34f3a88a8e8
okarslan <okarslan@dabnet.com.tr>
2025-07-26 14:31:07 +0300

    animation loop

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import Stats from 'three/addons/libs/stats.module.js'
 
 const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 1.5
 
-const renderer = new THREE.WebGLRenderer({ antialias: false })
+const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
@@ -17,7 +18,10 @@ window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight)
 })
 
-new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.addEventListener('change', function () {
+  renderer.render(scene, camera)
+})
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshNormalMaterial({ wireframe: true })
@@ -25,10 +29,24 @@ const material = new THREE.MeshNormalMaterial({ wireframe: true })
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
-function animate() {
-  requestAnimationFrame(animate)
+const stats = new Stats()
+document.body.appendChild(stats.dom)
 
-  renderer.render(scene, camera)
+
+// const clock = new THREE.Clock()
+// let delta
+function animate() {
+   requestAnimationFrame(animate)
+
+//   delta =clock.getDelta()
+
+//   cube.rotation.x +=  delta
+//   cube.rotation.y +=  delta
+
+//   renderer.render(scene, camera)
+
+  stats.update()
 }
 
-animate()
+ animate()
+renderer.render(scene, camera)
